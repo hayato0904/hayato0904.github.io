d156f70032e9c43f8989efb339fec7e3cb0ebbee
hayato0904 <66337508+hayato0904@users.noreply.github.com>
2020-08-24 10:03:27 +0900

    index.php

--- index.php
+++ index.php
@@ -15,7 +15,30 @@ try{
     $sql = 'select * from user';
     foreach ($dbh->query($sql) as $row) {
         print($row['id'].',');
-        print(!empty($row['name']));
+        print($row['user_name'].',');
+        print($row['place'].',');
+        print($row['language_html'].',');
+        print($row['language_css'].',');
+        print($row['language_javasprict'].',');
+        print($row['language_ruby'].',');
+        print($row['language_python'].',');
+        print($row['language_java'].',');
+        print($row['language_go'].',');
+        print($row['language_sql'].',');
+        print($row['language_c'].',');
+        print($row['language_c++'].',');
+        print($row['work_experience'].',');
+        print($row['my_age'].',');
+        print($row['you_hope_age_dont_worry'].',');
+        print($row['you_hope_age_10s'].',');
+        print($row['you_hope_age_early20s'].',');
+        print($row['you_hope_age_late20s'].',');
+        print($row['you_hope_age_early30s'].',');
+        print($row['you_hope_age_late30s'].',');
+        print($row['you_hope_age_40s'].',');
+        print($row['target'].',');
+        print($row['self_introduction'].',');
+        print($row['twitter'].',');
         print('<br />');
     }
 }catch (PDOException $e){
