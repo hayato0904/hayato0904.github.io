57d558003fcbaf9eb00eea9e7151f2a79abfd4db
hayato0904 <66337508+hayato0904@users.noreply.github.com>
2020-08-28 23:38:27 +0900

    見やすく改行＆index.phpの反映成功！！

--- index.php
+++ index.php
@@ -1,49 +1,129 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-<title>MariaDBへの接続テスト</title>
-</head>
-<body>
 <?php
+ 
+//データベース接続
 $dsn = 'mysql:dbname=user;host=localhost';
 $user = 'root';
 $password = 'Ha09041208!';
-try{
-    $dbh = new PDO($dsn, $user, $password);
-    $sql = 'select * from user';
-    foreach ($dbh->query($sql) as $row) {
-        print($row['id'].',');
-        print($row['user_name'].',');
-        print($row['place'].',');
-        print($row['language_html'].',');
-        print($row['language_css'].',');
-        print($row['language_javasprict'].',');
-        print($row['language_ruby'].',');
-        print($row['language_python'].',');
-        print($row['language_java'].',');
-        print($row['language_go'].',');
-        print($row['language_sql'].',');
-        print($row['language_c'].',');
-        print($row['language_c++'].',');
-        print($row['work_experience'].',');
-        print($row['my_age'].',');
-        print($row['you_hope_age_dont_worry'].',');
-        print($row['you_hope_age_10s'].',');
-        print($row['you_hope_age_early20s'].',');
-        print($row['you_hope_age_late20s'].',');
-        print($row['you_hope_age_early30s'].',');
-        print($row['you_hope_age_late30s'].',');
-        print($row['you_hope_age_40s'].',');
-        print($row['target'].',');
-        print($row['self_introduction'].',');
-        print($row['twitter'].',');
-        print('<br />');
-    }
-}catch (PDOException $e){
-    print('Error:'.$e->getMessage());
-    die();
+$dbh = new PDO($dsn,$user,$password);
+ 
+$sql = "SELECT * FROM user";
+ 
+$result = $dbh -> query($sql);
+ 
+//クエリー失敗
+if(!$result) {
+	echo $dbh->error;
+	exit();
 }
-$dbh = null;
+ 
+//レコード件数
+$row_cnt = $result->rowCount();
+ 
+///連想配列で取得
+$sth = $dbh -> query($sql);
+$aryList = $sth -> fetchAll(PDO::FETCH_ASSOC);
 ?>
+​
+​
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="/css/search.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>テスト用表示画面</title>
+  </head>
+  <body>
+    <h1>テスト用表示画面</h1>
+ 
+    <?php 
+    foreach($aryList as $aryList){
+    ?> 
+    <table border="1" width="80%" bordercolor="#green" bgcolor="#f5f5f5">
+        <tr bgcolor="deepskyblue">
+         <td>ユーザ名</td>
+        </tr>
+        <tr>
+         <td><?php echo htmlspecialchars($aryList['user_name'],ENT_QUOTES,'UTF-8'); ?></td>
+        </tr>
+        <tr bgcolor="deepskyblue">
+            <td>希望する活動場所</td>
+        </tr>
+        <tr>
+            <td><?php echo htmlspecialchars($aryList['place'],ENT_QUOTES,'UTF-8'); ?></td>  
+        </tr> 
+        <tr bgcolor="deepskyblue">
+            <td>使用できるプログラミング言語</td>
+        </tr>
+        <tr>
+            <td>
+                <?php echo htmlspecialchars($aryList['language_html'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_css'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_javasprict'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_ruby'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_python'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_java'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_go'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_sql'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_php'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_c'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['language_c++'].',',ENT_QUOTES,'UTF-8'); ?>
+            </td>  
+        </tr> 
+        <tr bgcolor="deepskyblue">
+            <td>実務経験歴</td>
+        </tr>
+        <tr>
+            <td><?php echo htmlspecialchars($aryList['work_experience'],ENT_QUOTES,'UTF-8'); ?></td>  
+        </tr> 
+        <tr bgcolor="deepskyblue">
+            <td>自分の年齢</td>
+        </tr>
+        <tr>
+            <td><?php echo htmlspecialchars($aryList['my_age'],ENT_QUOTES,'UTF-8'); ?></td>  
+        </tr> 
+        <tr bgcolor="deepskyblue">
+            <td>相手の希望年齢</td>
+        </tr>
+        <tr>
+            <td>
+                <?php echo htmlspecialchars($aryList['you_hope_age_dont_worry'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['you_hope_age_10s'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['you_hope_age_early20s'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['you_hope_age_late20s'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['you_hope_age_early30s'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['you_hope_age_late30s'].',',ENT_QUOTES,'UTF-8'); ?>
+                <?php echo htmlspecialchars($aryList['you_hope_age_40s'].',',ENT_QUOTES,'UTF-8'); ?>
+            </td>  
+        </tr> 
+        <tr bgcolor="deepskyblue">
+            <td>目標</td>
+        </tr>
+        <tr>
+            <td><?php echo htmlspecialchars($aryList['target'],ENT_QUOTES,'UTF-8'); ?></td>  
+        </tr>
+        <tr bgcolor="deepskyblue">
+            <td>自己紹介</td>
+        </tr>
+        <tr>
+            <td><?php echo htmlspecialchars($aryList['self_introduction'],ENT_QUOTES,'UTF-8'); ?></td>  
+        </tr>
+        <tr bgcolor="deepskyblue">
+            <td>自己紹介</td>
+        </tr>
+        <tr>
+            <td><?php echo htmlspecialchars($aryList['self_introduction'],ENT_QUOTES,'UTF-8'); ?></td>  
+        </tr>
+        <tr bgcolor="deepskyblue">
+            <td>Twitter</td>
+        </tr>
+        <tr>
+            <td><?php echo htmlspecialchars($aryList['twitter'],ENT_QUOTES,'UTF-8'); ?></td>  
+        </tr>
+    <br>
+    </table>
+    <?php 
+    } 
+    ?>
+ 
+</table>
+  </body>
+</html>
