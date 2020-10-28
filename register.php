fcd6048a653cfcdad231e3851fbf5339e609db22
hayato0904 <66337508+hayato0904@users.noreply.github.com>
2020-10-28 09:45:27 +0900

    自分のプロフィールのHTML側の画面　計画書通り実装

---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>新規登録　登録情報変更</title>
+</head>
+<body>
+	<h1>新規登録</h1>
+		<form action="" method="POST">
+			UserName<input type="text" name="username" value=""><br>
+			Password<input type="password" name="password" value=""><br>
+			<input type="submit" name="signup" value="新規登録">
+    </form>
+</body>
+</html>
