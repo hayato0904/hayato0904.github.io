c2c6a13c9947917db830db9ac4bdc92d0c36037a
hayato0904 <66337508+hayato0904@users.noreply.github.com>
2020-10-29 23:45:17 +0900

    新規登録

---
+++ signup.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>新規登録画面</title>
+</head>
+<body>
+	<h1>新規登録画面</h1>
+		<form action="" method="POST">
+			UserName<input type="text" name="username" value=""><br>
+			Password<input type="password" name="password" value=""><br>
+			<input type="submit" name="signup" value="新規登録">
+    </form>
+</body>
+</html>
